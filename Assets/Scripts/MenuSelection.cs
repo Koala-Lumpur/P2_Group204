27fa617aed94275bef682f5e20cd24019f31815b
Rasmus Nielsen <rewn97@gmail.com>
2018-04-05 13:18:11 +0200

    Basic menu system setup
    From the menu system you are able to look at an item and press the button to change scenes (only works for one since we only have on other scene), we have also started to emphasise the item you are looking at in the menu

---
+++ Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour {
+    private RaycastHit hit;
+
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+	void Update()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3 (0.5f, 0.5f, 0));
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            StartCoroutine(ScaleHit());
+            //hit.transform.LookAt(Camera.main.gameObject.transform.position);
+            if (hit.transform.gameObject.name == "Waterfall" && Input.GetMouseButton(0))
+            {
+                SceneManager.LoadScene("_MainScene", LoadSceneMode.Single);
+                Debug.Log("New level pls");
+            }
+        }
+
+    }
+    IEnumerator ScaleHit()
+    {
+        Vector3 startScale = hit.transform.localScale;
+        Vector3 targetScale = new Vector3(2.0f, 2.0f, 2.0f);
+        float scaleDuration = 5;
+
+        for (float t = 0; t < 1; t += Time.deltaTime / scaleDuration) {
+            hit.transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+                    yield return null;
+        }
+
+}
+}
