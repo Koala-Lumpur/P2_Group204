2b6331b25b26373bfff1a66534f3c0309a5ff830
Rasmus Nielsen <rewn97@gmail.com>
2018-04-26 14:24:47 +0200

    Added more details to terrain

---
+++ Assets/Scripts/DetailDistance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailDistance : MonoBehaviour {
+
+    public int detailDistance;
+    public Terrain terrain;
+
+	// Use this for initialization
+	void Start () {
+        terrain.detailObjectDistance = detailDistance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
