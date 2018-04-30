17f76382f631df93fe9e35041a85630284b04eef
Rasmus Nielsen <rewn97@gmail.com>
2018-04-30 17:22:43 +0200

    Added birds and start to Bird AI

---
+++ Assets/Scripts/BirdAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdAI : MonoBehaviour {
+
+    public Transform[] path;
+    public float speed;
+    public float reachDist;
+    public int currentPoint;
+    private Vector3 dir;
+
+
+    void Start()
+    {
+        dir = path[currentPoint].position - transform.position;
+    }
+
+    void Update()
+    {
+        
+
+        transform.position += dir * speed;
+
+        if(dir.magnitude <= reachDist)
+        {
+            PointReached();
+        }
+    }
+
+    void PointReached()
+    {
+        currentPoint++;
+        dir = path[currentPoint].position - transform.position;
+    }
+}
